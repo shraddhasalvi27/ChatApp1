f6fa99dc47f2818e1a89dafc4a849e6b73aca1b8
shraddha salvi <shraddhasalvi272@gmail.com>
2025-01-10 23:20:53 +0530

    added all frontend components and context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Left from "./home/Leftpart/Left";
+import Right from "./home/Rightpart/Right";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { useAuth } from "./context/AuthProvider";
+import { Toaster } from "react-hot-toast";
 
+import { Navigate, Route, Routes } from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            authUser ? (
+              
+              <div className="drawer lg:drawer-open">
+                <input
+                  id="my-drawer-2"
+                  type="checkbox"
+                  className="drawer-toggle"
+                />
+                <div className="drawer-content flex flex-col items-center justify-center">
+                  <Right />
+                </div>
+                <div className="drawer-side">
+                  <label
+                    htmlFor="my-drawer-2"
+                    aria-label="close sidebar"
+                    className="drawer-overlay"
+                  ></label>
+                  <ul className="menu w-80 min-h-full bg-black text-base-content">
+                    <Left />
+                  </ul>
+                </div>
+              </div>
+            ) : (
+              <Navigate to={"/login"} />
+            )
+          }
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <Signup />}
+        />
+      </Routes>
+      <Toaster />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthProvider.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { SocketProvider } from "./context/SocketContext.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <SocketProvider>
+        <App />
+      </SocketProvider>
+    </AuthProvider>
+  </BrowserRouter>
+);
