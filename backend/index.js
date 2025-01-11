6004959000457951970c8423b0bb17c1ad835ecd
shraddha salvi <shraddhasalvi272@gmail.com>
2025-01-11 11:15:54 +0530

    App completed now it's ready fro production

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
@@ -25,9 +25,19 @@ try {
     console.log(error);
 }
 
-//routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
+
+//----------------------code for deployment--------------------------
+if(process.env.NODE_ENV=="production"){
+    const dirPath = path.resolve();
+
+    app.use(express.static("./Frontend/dist"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(dirPath,"./Frontend/dist","index.html"));
+    })
+}
+
 
 server.listen(PORT, () => {
     console.log(`Server is Running on port ${PORT}`);
