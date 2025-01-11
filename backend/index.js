4dcf1c93db5f61830969f745d78d725c94c7ee3f
shraddha salvi <shraddhasalvi272@gmail.com>
2025-01-11 11:57:19 +0530

    few changes

--- backend/index.js
+++ backend/index.js
@@ -32,9 +32,9 @@ app.use("/api/message", messageRoute);
 if(process.env.NODE_ENV=="production"){
     const dirPath = path.resolve();
 
-    app.use(express.static("./Frontend/dist"));
+    app.use(express.static("./frontend/dist"));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(dirPath,"./Frontend/dist","index.html"));
+        res.sendFile(path.resolve(dirPath,"./frontend/dist","index.html"));
     })
 }
 
