bfde3f7c4250cc4302c9232477c4ff7d97ae4a81
shraddha salvi <shraddhasalvi272@gmail.com>
2025-01-24 15:31:12 +0530

    missed import

--- backend/frontend/src/components/Navbar.jsx
+++ backend/frontend/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-    return (
-      <header className="bg-blue-500 p-4">
-        <div className="flex justify-between items-center max-w-7xl mx-auto">
-          <Link to="/" className="flex items-center">
-            <img src="/ChatAppLogo.png" alt="Logo" className="h-10 w-auto" />
-            <span className="ml-2 font-bold text-white text-xl">Panchat</span>
-          </Link>
-        </div>
-      </header>
-    );
-  };
-  
+  return (
+    <header className="bg-blue-500 p-4">
+      <div className="flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="flex items-center">
+          <img src="/ChatAppLogo.png" alt="Logo" className="h-10 w-auto" />
+          <span className="ml-2 font-bold text-white text-xl">Panchat</span>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
 export default Navbar;
